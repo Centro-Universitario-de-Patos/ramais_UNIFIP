8b1cc1043d5b66fc555badef40248762597e01bc
Rodrigo Mendes <gerenciati@fiponline.edu.br>
2021-06-10 17:13:43 -0300

    Colocando o UNIDADE na pagina de admin #7

--- Unidade/admin.py
+++ Unidade/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Unidade
 # Register your models here.
+
+@admin.register (Unidade)
+
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('nome_unidade','cidade', 'campus', 'endereco')
+
+
