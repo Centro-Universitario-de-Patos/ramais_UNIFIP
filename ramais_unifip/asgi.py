0dfb585c8933340f205deff11702d8288f1f2b70
Rodrigo Mendes <gerenciati@fiponline.edu.br>
2021-06-10 11:16:55 -0300

    Comit criação do arcabouço do projeto.

---
+++ ramais_unifip/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ramais_unifip project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ramais_unifip.settings')
+
+application = get_asgi_application()
